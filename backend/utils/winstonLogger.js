afbfc22708ccdbaab5fada14d7ee23ec3e56b297
mikemey <miguel.maestro@gmx.co.uk>
2018-11-20 15:30:06 +0000

    added console logging in dev env.

--- backend/utils/winstonLogger.js
+++ backend/utils/winstonLogger.js
@@ -2,12 +2,23 @@ const winston = require('winston')
 
 const { createLogger, format, transports } = winston
 
+const prodEnvironment = process.env.NODE_ENV === 'PROD'
+
 const WinstonLogger = filename => {
+  const output = prodEnvironment
+    ? new transports.File({ filename })
+    : new transports.Console({ format: format.simple() })
+
   const winston = createLogger({
     format: format.printf(info => info.message),
-    transports: [new transports.File({ filename })]
+    transports: output
   })
 
+  if (!prodEnvironment) {
+    winston.info('!!!!!!!!!!!!!!!!!!!!')
+    winston.info('STARTING IN DEV MODE')
+    winston.info('!!!!!!!!!!!!!!!!!!!!')
+  }
   return {
     info: msg => winston.info(msg),
     log: obj => winston.info(JSON.stringify(obj, null, ' ')),
