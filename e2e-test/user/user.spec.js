e41add19fcfcd59f2913914a91beb8ce48c96d38
mikemey <miguel.maestro@gmx.co.uk>
2018-12-01 20:44:46 +0000

    added search form label and button.

--- e2e-test/user/user.spec.js
+++ e2e-test/user/user.spec.js
@@ -11,9 +11,19 @@ describe('User main page', () => {
       expect(status.getText()).toEqual('ready')
     })
 
+    it('should show userId label', () => {
+      const userInputLabel = element(by.xpath('//form/label'))
+      expect(userInputLabel.getText()).toEqual('User ID:')
+    })
+
     it('should show userId input', () => {
       const userInput = element(by.name('userId'))
       expect(userInput.isDisplayed()).toBeTruthy()
+    })
+
+    it('should show search postings button', () => {
+      const searchButton = element(by.xpath('//form/button'))
+      expect(searchButton.isDisplayed()).toBeTruthy()
     })
   })
 })
