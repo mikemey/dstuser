daee0801c54cf1b7b8bdea920280a4eab18128e6
mikemey <miguel.maestro@gmx.co.uk>
2018-12-02 10:26:58 +0000

    more specific search-page element identifiers.

--- e2e-test/user/searchPage.js
+++ e2e-test/user/searchPage.js
@@ -3,10 +3,10 @@ const { by, browser, element } = require('protractor')
 const SearchPage = {
   open: () => browser.get('./'),
   statusText: () => element(by.id('status')).getText(),
-  searchLabel: () => element(by.xpath('//form/label')).getText(),
+  searchUserLabel: () => element(by.xpath('//form/label')).getText(),
 
-  searchButton: () => element(by.xpath('//form/button')),
-  searchInput: () => element(by.name('userId'))
+  searchUserButton: () => element(by.xpath('//form/button')),
+  searchUserInput: () => element(by.name('userId'))
 }
 
 module.exports = SearchPage
--- e2e-test/user/user.spec.js
+++ e2e-test/user/user.spec.js
@@ -2,22 +2,22 @@ const SearchPage = require('./searchPage')
 
 describe('User main page', () => {
   describe('static elements', () => {
-    beforeEach(SearchPage.open)
+    beforeAll(SearchPage.open)
 
     it('should show status message "ready"', () => {
       expect(SearchPage.statusText()).toEqual('ready')
     })
 
     it('should show userId label', () => {
-      expect(SearchPage.searchLabel()).toEqual('User ID:')
+      expect(SearchPage.searchUserLabel()).toEqual('User ID:')
     })
 
     it('should show userId input', () => {
-      expect(SearchPage.searchInput().isDisplayed()).toBeTruthy()
+      expect(SearchPage.searchUserInput().isDisplayed()).toBeTruthy()
     })
 
     it('should show search postings button', () => {
-      expect(SearchPage.searchButton().isDisplayed()).toBeTruthy()
+      expect(SearchPage.searchUserButton().isDisplayed()).toBeTruthy()
     })
   })
 })
