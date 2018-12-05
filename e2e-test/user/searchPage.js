4a1089f1fb1c236a31b375423ee7a1a070968ca7
mikemey <miguel.maestro@gmx.co.uk>
2018-12-05 15:58:43 +0000

    design update comments.

--- e2e-test/user/searchPage.js
+++ e2e-test/user/searchPage.js
@@ -21,20 +21,20 @@ const byErrorMessage = by.id('errorMessage')
 const hasErrorMessage = () => browser.isElementPresent(byErrorMessage)
 const getErrorMessage = () => element(byErrorMessage).getText()
 
-const getComments = () => element.all(by.className('comment-box'))
+const getComments = () => element.all(by.className('cmnt-box'))
   .map(createComment)
 
 const createComment = el => {
-  const titleEl = el.element(by.className('comment-title'))
-  const urlEl = el.element(by.className('comment-url'))
-  const contentEl = el.element(by.className('comment-content'))
-  const articleEl = el.element(by.className('comment-article'))
+  const titleEl = el.element(by.className('cmnt-title'))
+  const urlEl = el.element(by.className('cmnt-url'))
+  const contentEl = el.element(by.className('cmnt-content'))
+  const articleEl = el.element(by.className('cmnt-article'))
   return {
     title: titleEl.getText(),
-    url: urlEl.getAttribute('href'),
+    url: urlEl.getAttribute('ng-href'),
     content: contentEl.getText(),
     articleTitle: articleEl.getText(),
-    articleUrl: articleEl.getAttribute('href')
+    articleUrl: articleEl.getAttribute('ng-href')
   }
 }
 
