b30cc2eaf8190d68acc9aeeb47eca6991fc29f35
mikemey <miguel.maestro@gmx.co.uk>
2018-12-07 21:26:45 +0000

    separated user page tests into static/dynamic parts.

---
+++ e2e-test/user/user.search.part.js
@@ -0,0 +1,75 @@
+const { Key } = require('protractor')
+
+const derStandard = require('../derStandardMock')
+
+module.exports = searchPage => {
+  const hideElementsScreen = searchPage.getHiddenScreen()
+
+  describe(`[${searchPage.id}]: user search (single comment page)`, () => {
+    const userId = '755005'
+    const pageNum = 1
+
+    beforeAll(() => {
+      derStandard.start()
+      derStandard.serveUserPageFor(userId, pageNum)
+    })
+
+    afterAll(derStandard.stop)
+
+    beforeEach(searchPage.open)
+
+    it('should forward to user page', () => {
+      searchPage.requestUserComments(userId)
+      expect(searchPage.getBrowserUrl()).toMatch(new RegExp(`/search/${userId}$`))
+    })
+
+    it('should show user name', () => {
+      expect(searchPage.hasUserName()).toBeFalsy()
+      searchPage.requestUserComments(userId)
+      expect(searchPage.getUserName()).toEqual('a standard user')
+      expect(searchPage.hasUserName(hideElementsScreen)).toBeFalsy()
+    })
+
+    it('should start request when Key.ENTER pressed', () => {
+      searchPage.setUserId(userId)
+      searchPage.setUserId(Key.ENTER)
+      expect(searchPage.getUserName()).toEqual('a standard user')
+    })
+
+    it('should request comments when URL contains userId', () => {
+      searchPage.open(`#!/search/${userId}`)
+      expect(searchPage.getUserId()).toEqual(userId)
+      expect(searchPage.getUserName()).toEqual('a standard user')
+    })
+
+    it('should show user comments', async () => {
+      const expectedPostings = derStandard.getCommentResult(userId).postings
+      searchPage.requestUserComments(userId)
+      const comments = await searchPage.getComments()
+
+      expectedPostings.forEach((expected, ix) => {
+        expect(comments[ix].title).toBe(expected.title, `title ix: ${ix}`)
+        expect(comments[ix].content).toBe(expected.content, `content ix: ${ix}`)
+        expect(comments[ix].url).toBe(expected.url, `url ix: ${ix}`)
+        expect(comments[ix].articleTitle).toBe(expected.article.title, `article.title ix: ${ix}`)
+        expect(comments[ix].articleUrl).toBe(expected.article.url, `article.url ix: ${ix}`)
+      })
+    })
+  })
+
+  describe(`[${searchPage.id}]: derStandard errors`, () => {
+    const userId = '755005'
+    beforeEach(derStandard.start)
+    afterEach(derStandard.stop)
+
+    it('should show error message', () => {
+      derStandard.server404WhenUserPageFor(userId)
+      searchPage.open()
+      searchPage.requestUserComments(userId)
+
+      expect(searchPage.hasErrorMessage()).toBeTruthy()
+      expect(searchPage.hasErrorMessage(hideElementsScreen)).toBeFalsy()
+      expect(searchPage.getErrorMessage()).toEqual(`User ID not found: ${userId}`)
+    })
+  })
+}
