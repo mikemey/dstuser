d41cc1c659049e811add165fa22e122fcd222dc2
mikemey <miguel.maestro@gmx.co.uk>
2018-12-19 21:30:57 +0000

    added multiple user page karma test (partial karma points).

--- e2e-test/user/karma.spec.part.js
+++ e2e-test/user/karma.spec.part.js
@@ -1,16 +1,16 @@
 const derStandard = require('../derStandardMock')
 
 module.exports = searchPage => {
-  const userId = '755005'
-  const postingIds = [1034153378, 1034368216, 1036279475]
+  const smallUserId = '755005'
+  const userId = '425185'
 
   describe('karma points', () => {
     beforeAll(() => {
       derStandard.start()
+      derStandard.serveUserPageFor(smallUserId, 1)
       derStandard.serveUserPageFor(userId, 1)
-      postingIds.forEach(postingId =>
-        derStandard.serveRating(userId, postingId)
-      )
+      derStandard.serveUserPageFor(userId, 2)
+      derStandard.serveUserPageFor(userId, 3)
     })
 
     afterAll(derStandard.stop)
@@ -20,10 +20,15 @@ module.exports = searchPage => {
       expect(searchPage.hasKarma()).toBeFalsy()
     })
 
-    it('shows cumulative ratings', () => {
-      searchPage.openUserPage(userId)
+    it('shows cumulative ratings (single user page)', () => {
+      searchPage.openUserPage(smallUserId)
       expect(searchPage.hasKarma()).toBeTruthy()
       expect(searchPage.getKarmaPoints()).toEqual([17, 18])
+    })
+
+    it('shows cumulative ratings (multiple user page)', () => {
+      searchPage.openUserPage(userId)
+      expect(searchPage.getKarmaPoints()).toEqual([278, 231])
     })
   })
 }
