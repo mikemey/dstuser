99c3d11133c33c8cf73f41e2af1fb9467752b7c7
mikemey <miguel.maestro@gmx.co.uk>
2018-12-22 09:47:18 +0000

    part 2: separating user-spec parts into own directory.

--- e2e-test/user.spec.js
+++ e2e-test/user.spec.js
@@ -1,10 +1,10 @@
-const { SearchPage, ALL_SCREENS } = require('../pageobjects/search.page')
+const { SearchPage, ALL_SCREENS } = require('./pageobjects/search.page')
 
-const htmlTests = require('./html.spec.part')
-const searchUserTests = require('./search.spec.part')
-const filterCommentsTests = require('./filter.spec.part')
-const ratingsTests = require('./rating.spec.part')
-const statsTests = require('./stats.spec.part')
+const htmlTests = require('./user-spec-parts/html.spec.part')
+const searchUserTests = require('./user-spec-parts/search.spec.part')
+const filterCommentsTests = require('./user-spec-parts/filter.spec.part')
+const ratingsTests = require('./user-spec-parts/rating.spec.part')
+const statsTests = require('./user-spec-parts/stats.spec.part')
 
 ALL_SCREENS.forEach(testScreen => {
   const searchPage = SearchPage(testScreen)
