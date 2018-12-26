f7b7b14df49ec471f5852e07c80b5057d9b6bb08
mikemey <miguel.maestro@gmx.co.uk>
2018-12-26 19:16:10 +0000

    part 2: consistent naming of components + modules.

--- frontend/app/user-postings/user-postings.module.js
+++ frontend/app/user-postings/user-postings.module.js
@@ -1,11 +1,11 @@
 import angular from 'angular'
 
-import postings from './postings.comp'
+import postingsWall from './postings-wall.comp'
 import postingRating from './posting-rating.comp'
 import pageLinks from './page-links.comp'
 
 export default angular.module('user.postings', [
-  postings.name,
+  postingsWall.name,
   postingRating.name,
   pageLinks.name
 ])
